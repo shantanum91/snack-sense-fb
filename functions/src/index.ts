847492614d5326870070e293a7829977160a38d9
Shantanu Mahakale <shantanum91@gmail.com>
2026-03-20 07:56:27 +0530

    Initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+export const getAppVersion = onRequest({ cors: true }, (req, res) => {
+    logger.info("Retriving app version", { structuredData: true });
+    res.json({ latestVersion: "1.0.2" });
+});
